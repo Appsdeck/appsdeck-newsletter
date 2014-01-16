14f622b36393b9adc6d9ed948f0710c659498eb4
Yann Klis <yann.klis@novelys.com>
2014-01-16 15:55:02 +0100

    january newsletter

--- render.rb
+++ render.rb
@@ -5,17 +5,19 @@ require "rss"
 
 @today = Time.now
 
+last = Time.new(2013, 11, 1)
+
 url = 'http://feeds.feedburner.com/HelloAppsdeck?format=xml'
 uri = URI.parse(url)
 feed = uri.read
 rss = RSS::Parser.parse(feed, false, true)
-@posts = rss.items
+@posts = rss.items.delete_if{|x| x.pubDate < last }
 
 url = 'http://feeds.delicious.com/v2/rss/strass/appsdeck+business'
 uri = URI.parse(url)
 feed = uri.read
 rss = RSS::Parser.parse(feed, false, true)
-@articles = rss.items
+@articles = rss.items.delete_if{|x| x.pubDate < last }
 
 template = File.read("newsletter.html.erb")
 renderer = ERB.new(template)
